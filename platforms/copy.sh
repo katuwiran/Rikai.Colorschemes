aee1dfd24a05bfd324ffad5556c7cec50b0fa206
K. Rikaisuru <shirurururu@outlook.com>
2026-03-08 00:56:04 +0800

    feat: script to copy configs to their folders

---
+++ platforms/copy.sh
@@ -0,0 +1,5 @@
+cp -f ./Alacritty/* ~/.config/alacritty
+cp -f ./Konsole/* ~/.local/share/konsole
+cp -rf ./Kvantum/* ~/.config/Kvantum/
+cp -rf ./Noctalia/* ~/.config/noctalia/colorschemes/
+cp -f ./Sublime\ Text/* ~/.config/sublime-text/Packages/User/
